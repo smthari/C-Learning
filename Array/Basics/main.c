2e930766a6ca39edbeb8d28fc7e1196b940e8168
sumit <smtharijan@gmail.com>
2022-12-06 16:54:12 +0530

    array accesing element and one dimensional example 2

--- Array/Basics/main.c
+++ Array/Basics/main.c
@@ -4,13 +4,29 @@ int main()
 {
 
     // array declaration
-    int a[2]; // a[0] , a[1]
+    /* int a[2]; // a[0] , a[1]
     printf("%i \n", a[0] = 25);
-    printf("%i \n", a[1] = 35);
+    printf("%i \n", a[1] = 35); */
 
     // int a[2] = {25, 35};
     // printf("%i \n", a[0]);
     // printf("%i \n", a[1]);
 
-    return 0;
+    // accessing array element - one dimensional array
+    /* int a[3] = {23, 16, 36};
+    printf("%d \n", a[0]); // 23
+    printf("%d \n", a[1]); // 16
+    printf("%d \n", a[2]); // 36 */
+
+    // accessing array element - two dimensional array
+    int a[2][3] = {45, 65, 12, 5, 6, 4};
+
+    printf("%d \n", a[0][0]); // 23
+    printf("%d \n", a[0][1]); // 16
+    printf("%d \n", a[0][2]); // 36
+    printf("%d \n", a[1][0]); // 36
+    printf("%d \n", a[1][1]); // 36
+    printf("%d \n", a[1][2]); // 36
+
+        return 0;
 }
---
+++ Array/One dimensional/example2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// one dimensional array example 2
+int main()
+{
+    int a[2] = {52, 32}; // a[0] =52 , a[1] = 32
+    for (int i = 0; i < 2; i++)
+    {
+        printf("%i\n", a[i]);
+    }
+
+    return 0;
+}
