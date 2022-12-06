efa3f2bf5f774b08e0124f7ea731a7d2dd92a51e
sumit <smtharijan@gmail.com>
2022-12-06 18:38:01 +0530

    we can Initialize array element in different ways

--- Array/Basics/main.c
+++ Array/Basics/main.c
@@ -19,14 +19,33 @@ int main()
     printf("%d \n", a[2]); // 36 */
 
     // accessing array element - two dimensional array
-    int a[2][3] = {45, 65, 12, 5, 6, 4};
+    /* int a[2][3] = {45, 65, 12, 5, 6, 4};
 
     printf("%d \n", a[0][0]); // 23
     printf("%d \n", a[0][1]); // 16
     printf("%d \n", a[0][2]); // 36
     printf("%d \n", a[1][0]); // 36
     printf("%d \n", a[1][1]); // 36
-    printf("%d \n", a[1][2]); // 36
+    printf("%d \n", a[1][2]); // 36 */
 
-        return 0;
+    // Ways to Initializing Two – Dimensional Array
+    // first method
+    // int a[2][3] = {45,65,12,5,6,4};
+
+    // second method
+    // int a[2][3] = {{45, 65, 12}, {5, 6, 4}};
+
+    // third method using for loop
+    int a[2][3];
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            printf("Enter value :");
+            scanf("%d", &a[i][j]);
+            printf("%i\n", a[i][j]);
+        }
+    }
+
+    return 0;
 }
--- Array/Two dimensiona/example1.c
+++ Array/Two dimensiona/example1.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 
 // two dimensional array example 1
-int main() {
-    int a[2][3] = {45,65,12,5,6,4};
-    for (int i=0; i<2; i++) {
-        for (int j=0; j<3; j++) {
+int main()
+{
+    // int a[2][3] = {45,65,12,5,6,4};
+    int a[2][3] = {{45, 65, 12}, {5, 6, 4}};
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
             // printf("Enter value :");
             // scanf("%d", &a[i][j]);
             // printf("%i\n", a[i][j]);
-            printf("a[%d] [%d] = %d \n",i,j,a[i][j]);    
+            printf("a[%d] [%d] = %d \n", i, j, a[i][j]);
         }
-        
     }
     return 0;
 }
