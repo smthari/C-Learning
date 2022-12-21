bc35a09c146885f4d07e52d3e12dd051b724b7aa
sumit <smtharijan@gmail.com>
2022-12-21 20:16:14 +0530

    armstrong number updated

--- Assignments/print-armstrong-no.c
+++ Assignments/print-armstrong-no.c
@@ -1,32 +1,56 @@
+
+// 🗒️ write a program to print armstrong number from the range 1 to 1000 ✅
 #include <stdio.h>
 
 int main()
 {
-    int i, num, x, sum, JALLAD;
+    int i, n, x, sum, armstrong;
     for (i = 1; i <= 1000; i++)
     {
         sum = 0;
-        num = i;
-        JALLAD = num;
+        n = i;
+        armstrong = n;
 
-        while (num > 0)
+        while (n > 0)
         {
-            x = num % 10;
-            num = num / 10;
+            x = n % 10;
+            n = n / 10;
             sum = sum + x * x * x;
         }
-        if (sum == JALLAD)
+        if (sum == armstrong)
         {
-            printf("THE ARMSTRONG NUMBER : %d\n", JALLAD);
+            printf("Armstrong number : %d\n", armstrong);
+        }
+    }
+    return 0;
+}
+
+// for easy understanding - print armstrong number from 152 to 153
+/* #include <stdio.h>
+
+int main()
+{
+    int i, n, x, sum, armstrong;
+    for (i = 152; i <= 153; i++)
+    {
+        sum = 0;
+        n = i;
+        armstrong = n;
+
+        while (n > 0)
+        {
+            x = n % 10;
+            n = n / 10;
+            sum = sum + x * x * x;
+            // printf("x %d \n", x);
+            // printf("n= %d \n", n);
+            // printf("sum %d \n", sum);
+        }
+        if (sum == armstrong)
+        {
+            printf("THE ARMSTRONG NUMBER : %d\n", armstrong);
         }
     }
 
     return 0;
-}
-
-// Output
-/* THE ARMSTRONG NUMBER : 1
-THE ARMSTRONG NUMBER : 153
-THE ARMSTRONG NUMBER : 370
-THE ARMSTRONG NUMBER : 371
-THE ARMSTRONG NUMBER : 407 */
+} */
