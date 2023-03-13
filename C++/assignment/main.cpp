b5a652d781fcf7f2da189b57e80ccf0a669f137b
sumit <smtharijan@gmail.com>
2023-03-13 22:25:22 +0530

    class assignment questions

---
+++ C++/assignment/main.cpp
@@ -0,0 +1,64 @@
+// ❓Take input for five subjects find out the average and print fail if total marks<150 else pass
+/* #include <iostream>
+using namespace std;
+
+class average
+{
+public:
+int s1,s2,s3,s4,s5;
+
+void calcAverage() {
+        int total = s1 + s2 + s3 + s4 + s5 ;
+        cout << total/5;
+
+    }
+};
+
+
+
+int main() {
+
+    average avg1;
+    avg1.s1 = 10;
+    avg1.s2 = 10;
+    avg1.s3 = 10;
+    avg1.s4 = 10;
+    avg1.s5 = 10;
+
+    // average calculation
+    avg1.calcAverage(); // 10
+
+    return 0;
+} */
+
+///////////////////////////////
+
+// ❓convert temperature from celsius to Fahrenheit
+// formula = Celsius * 1.8 + 32
+
+/* #include <iostream>
+using namespace std;
+
+class conversion
+{
+public:
+    int celsius;
+
+    void toFah()
+    {
+        float toFah = celsius * 1.8 + 32;
+        cout << toFah;
+    }
+};
+
+int main()
+{
+
+    conversion tempToCel;
+    tempToCel.celsius = 10;
+
+    // average calculation
+    tempToCel.toFah(); // 50
+
+    return 0;
+} */
